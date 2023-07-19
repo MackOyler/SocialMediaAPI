0b0d338646f312be0e8dd6c9b148f733e0af786d
Mack Oyler <120589371+MackOyler@users.noreply.github.com>
2023-07-19 13:34:34 -0500

    Update user-controller.js

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,7 +1,8 @@
 const { User, Thought } = require("../models");
 
 const userController = {
-  // get all users
+
+  
   getAllUser(req, res) {
     User.find({})
       .populate({
@@ -17,7 +18,8 @@ const userController = {
       });
   },
 
-  // get one user by id
+
+  
   getUserById({ params }, res) {
     User.findOne({ _id: params.id })
       .populate({
@@ -43,14 +45,16 @@ const userController = {
       });
   },
 
-  // create user
+
+  
   createUser({ body }, res) {
     User.create(body)
       .then((dbUserData) => res.json(dbUserData))
       .catch((err) => res.json(err));
   },
 
-  // update user by id
+
+  
   updateUser({ params, body }, res) {
     User.findOneAndUpdate({ _id: params.id }, body, {
       new: true,
@@ -66,7 +70,8 @@ const userController = {
       .catch((err) => res.json(err));
   },
 
-  // delete user
+
+  
   deleteUser({ params }, res) {
     User.findOneAndDelete({ _id: params.id })
       .then((dbUserData) => {
@@ -83,7 +88,8 @@ const userController = {
       .catch((err) => res.json(err));
   },
 
-  // add friend
+
+  
   addFriend({ params }, res) {
     User.findOneAndUpdate(
       { _id: params.userId },
@@ -100,7 +106,8 @@ const userController = {
       .catch((err) => res.json(err));
   },
 
-  // delete friend
+
+  
   removeFriend({ params }, res) {
     User.findOneAndUpdate(
       { _id: params.userId },
